27f84dd6e6cd287d2e02f78ca5f73c1d300f54e2
Huazhou Wang <plause@gmail.com>
2016-03-29 23:07:39 +0800

    Add handler for the whoami API

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 var http = require('http');
 
+function whoamiApiHandler(req, res) {
+	var languages = req.headers['accept-language'];
+	var comma = languages.indexOf(',');
+	var language = comma === -1 ? languages : languages.substring(0, comma);
+
+	var ua = req.headers['user-agent'];
+	var left = ua.indexOf('(');
+	var right = ua.indexOf(')');
+	var software = ua.substring(left + 1, right);
+
+	res.setHeader('Content-Type', 'application/json; charset=utf-8');
+	res.end(JSON.stringify({
+		'ipaddress': req.socket.remoteAddress,
+		'language': language,
+		'software': software
+	}));
+}
+
 function defaultHandler(req, res) {
 	res.statusCode = 404;
 	res.end();
 }
 
-var handlers = {};
+var handlers = {
+	'/api/whoami/': whoamiApiHandler
+};
 
 http.createServer(function (req, res) {
 	(handlers[req.url] || defaultHandler)(req, res);
